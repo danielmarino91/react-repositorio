d724d649524f89ace3f76b65ae817a518fd0c650
danielmarino91 <danielpibot@gmail.com>
2022-03-24 00:16:04 -0300

    Se agrego el link al widget del carrito

--- appfolder/src/components/NavBar.js
+++ appfolder/src/components/NavBar.js
@@ -1,13 +1,13 @@
 import Links from "./Links"
 import CartWidget from "./CartWidget"
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 function NavBar() {
     return (
         <nav className="navbar">
             <Link to="/"><img src={"/assets/kyotogameslogo.png"} alt="Kyoto Games Logo" /></Link>
             <Links />
-            <CartWidget numero="0" />
+            <NavLink to="/carrito"><CartWidget numero="0" /></NavLink>
         </nav>
     )
 }
