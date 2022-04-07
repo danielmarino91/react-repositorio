b5c29a5d9418e23261062c0c9da590d3675d33b9
danielmarino91 <danielpibot@gmail.com>
2022-04-07 01:07:58 -0300

    Se hicieron cambios de diseño en los items

--- appfolder/src/components/Item.js
+++ appfolder/src/components/Item.js
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom"
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 function Item({ producto }) {
 
     return (
         <Link to={`/item/${producto.id}`}>
-            <div className='itemStyle'>
-                <h3>{producto.nombre}</h3>
+            <div className='itemListStyle__item '>
                 <img src={producto.imagen}></img>
-                <div className="gameBadge"><span className="gameBadge__price">${producto.precio}</span><span className="gameBadge__genre">{producto.categoria}</span></div>
-                <div className="stockAvaible">Stock disponible: {producto.stock}</div>
+                <h3 className="itemListStyle__item--title">{producto.nombre}</h3>
+                <div className="itemListStyle__item--price">${producto.precio}</div>
+                <div className="itemListStyle__item--btnBuy"><AiOutlineShoppingCart />Comprar</div>
             </div>
         </Link>
     )
--- appfolder/src/components/Loader.js
+++ appfolder/src/components/Loader.js
@@ -6,7 +6,7 @@ const Loader = () => {
             <g className="circles">
                 <circle cx="125" cy="125" r="50" fill="none" strokeWidth="15px" stroke="#E0E3E9">
                 </circle>
-                <circle id="inner" cx="125" cy="125" r="50" fill="none" strokeWidth="12px" stroke="#6B04A8">
+                <circle id="inner" cx="125" cy="125" r="50" fill="none" strokeWidth="12px" stroke="#94c132">
                 </circle>
             </g>
         </svg>
